612d0d15b200f4d31d4ce3d944bcfb4db1c9d5f7
Yuuki Rika <yvvki@duck.com>
2023-08-27 12:57:24 +0700

    Initialize userscript

---
+++ user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name        SkyMods Helper for Steam Workshop
+// @namespace   https://github.com/yvvki/SkyMods-Helper
+// @match       *://steamcommunity.com/workshop/filedetails/?id=*
+// @match       *://steamcommunity.com/sharedfiles/filedetails/?id=*
+// @grant       none
+// @version     0.1.0
+// @author      Yuuki Rika
+// @license     AGPL-3.0-or-later
+// @description Easily download collections and items directly from Steam Workshop via smods.ru
+// @icon        https://steamcommunity.com/favicon.ico
+// ==/UserScript==
+
+/*
+ * Copyright (C) 2023  Yuuki Rika
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
