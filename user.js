591fbca9f165fcf41e84a81a901296ae3eee3766
Yuuki Rika <yvvki@duck.com>
2023-08-28 13:37:09 +0700

    Add button

--- user.js
+++ user.js
@@ -27,3 +27,71 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+// This script is inspired by https://github.com/cappig/steam-workshop-downloader
+
+"use strict";
+
+const appid = document
+  .querySelector(".apphub_sectionTab")
+  ?.getAttribute("href")
+  ?.split("/")[4];
+const parser = new DOMParser();
+
+var path = ""; // subfolder for collection mods, this only works on firefox, on Chrome it's included in the name
+
+var button; // Store the button so that we don't have to call querySelector() all the time
+
+// Run this on load and inject the HTML download button
+(function () {
+  button = document.createElement("span"); // global variable
+
+  let icon = button.appendChild(document.createElement("div"));
+  let text = button.appendChild(document.createElement("span"));
+
+  text.className = "subscribeText";
+  icon.className = "subscribeIcon";
+  icon.style.backgroundImage = "linear-gradient(#fff, #aaa)";
+  icon.style.maskImage = "url('https://raw.githubusercontent.com/google/material-design-icons/master/symbols/web/download/materialsymbolsoutlined/download_wght500grad200_20px.svg')";
+  icon.style.maskRepeat = "no-repeat";
+  icon.style.maskPosition = "center";
+  icon.style.width = "20px";
+  icon.style.left = "5px";
+
+  var fetchFunction;
+
+  if (document.querySelector(".collectionNotifications")) { // Collection
+    return; // Add support later
+
+    button.className = "general_btn subscribe";
+
+    text.innerText = "Download all";
+
+    fetchFunction = fetchCollection;
+
+    let name = document.querySelector(".workshopItemTitle")?.innerHTML;
+    path = `${name?.replace(/ /g, "_")}/`;
+  } else { // Item
+    button.className = "btn_green_white_innerfade btn_border_2px btn_medium ";
+    button.style.position = "relative";
+
+    text.innerText = "Download\u2003";
+
+    fetchFunction = fetchSingle;
+  }
+
+  button.addEventListener("click", fetchFunction);
+
+  let parentNode = document.querySelector("#SubscribeItemBtn")?.parentElement;
+  parentNode?.insertBefore(button, parentNode?.lastElementChild);
+})();
+
+function fetchSingle() {
+  console.debug("Fetching single workshop item");
+  // ...
+}
+
+function fetchCollection() {
+  console.debug("Fetching workshop item collection");
+  // ...
+}
